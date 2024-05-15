4a934f45d4d8e8d07c9c3cc2201a045ca454122e
hashiiiii <pkj59wwj@gmail.com>
2024-05-15 23:11:27 +0900

    add: 第六問

---
+++ workspace/abc088/b/main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	w  = bufio.NewWriter(os.Stdout)
+)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	defer flush()
+
+	// Implement
+	n := scanAsInt()
+	array := make([]int, n)
+	for i := 0; i < n; i++ {
+		array[i] = scanAsInt()
+	}
+
+	sortedArray := mergeSort(array, false)
+	a := 0
+	b := 0
+	for i := 0; i < n; i++ {
+		if i%2 == 0 {
+			a += sortedArray[i]
+		} else {
+			b += sortedArray[i]
+		}
+	}
+	_, err := fmt.Fprintln(w, a-b)
+	if err != nil {
+		panic(err)
+	}
+}
+
+/////////////////////////////
+// Scan
+/////////////////////////////
+
+func scanAsInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		_ = fmt.Errorf("[scanAsInt] error scanning integer: %s", e)
+	}
+	return i
+}
+
+func scanAsSplitInt(digit int) []int {
+	sc.Scan()
+	input := sc.Text()
+
+	var arr []int
+	if len(input) < digit {
+		arr = make([]int, digit)
+	} else {
+		arr = make([]int, len(input))
+	}
+
+	if len(arr) == 0 {
+		return arr
+	}
+
+	offset := len(arr) - len(input)
+	if offset < 0 {
+		_ = fmt.Errorf("[scanAsSplitInt] offset too small: %d", offset)
+	}
+
+	for i, c := range input {
+		var e error
+		arr[offset+i], e = strconv.Atoi(string(c))
+		if e != nil {
+			_ = fmt.Errorf("[scanAsSplitInt] error scanning integer: %s", e)
+		}
+	}
+	return arr
+}
+
+func scanAsString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+// ///////////////////////////
+// Sort
+// ///////////////////////////
+func mergeSort(unsorted []int, isAsc bool) []int {
+	if len(unsorted) < 2 {
+		return unsorted
+	}
+
+	mid := len(unsorted) / 2
+
+	// 再帰的に関数を実行してソート済のスライスを返す
+	left := mergeSort(unsorted[:mid], isAsc)
+	right := mergeSort(unsorted[mid:], isAsc)
+
+	sorted := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+
+	// マージ
+	for i < len(left) && j < len(right) {
+		if (isAsc && left[i] <= right[j]) || (!isAsc && left[i] >= right[j]) {
+			sorted = append(sorted, left[i])
+			i++
+		} else {
+			sorted = append(sorted, right[j])
+			j++
+		}
+	}
+
+	// 残りの要素を追加
+	sorted = append(sorted, left[i:]...)
+	sorted = append(sorted, right[j:]...)
+
+	return sorted
+}
+
+/////////////////////////////
+// Config
+/////////////////////////////
+
+func flush() {
+	e := w.Flush()
+	if e != nil {
+		_ = fmt.Errorf("[flush] error flushing: %s", e)
+	}
+}
