ccc41d1dc9c517161a3f316c1ae31d580bd854ad
hashiiiii <pkj59wwj@gmail.com>
2025-06-15 15:36:23 +0900

    abc410

---
+++ workspace/abc410/c/main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+var (
+	sc = bufio.NewScanner(os.Stdin)
+	w  = bufio.NewWriter(os.Stdout)
+)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+	sc.Split(bufio.ScanWords)
+}
+
+func main() {
+	defer flush()
+
+	// Implement
+	n, q := scanAsInt(), scanAsInt()
+	a := make([]int, n)
+	head := 0 // 最大は n - 1
+	for i := 0; i < n; i++ {
+		a[i] = i + 1
+	}
+	// --
+	flag := false
+	for i := 0; i < q; i++ {
+		x, y, z := scanAsInt(), scanAsInt(), 0
+		if x == 1 {
+			z = scanAsInt()
+			a[(head+y-1)%n] = z
+		} else if x == 2 {
+			flag = true
+			fmt.Fprintf(w, "%d\n", a[(head+y-1)%n])
+		} else if x == 3 {
+			head = (head + y) % n
+			//fmt.Fprintf(w, "%d\n", head)
+		}
+	}
+	if !flag {
+		fmt.Println("")
+	}
+}
+
+/////////////////////////////
+// Scan5_5
+//2_3
+//1_2_1000000
+//3_4
+//2_2
+//2_3
+/////////////////////////////
+
+func scanAsInt() int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		_ = fmt.Errorf("[scanAsInt] error scanning integer: %s", e)
+	}
+	return i
+}
+
+func scanAsSplitInt(digit int) []int {
+	sc.Scan()
+	input := sc.Text()
+
+	var arr []int
+	if len(input) < digit {
+		arr = make([]int, digit)
+	} else {
+		arr = make([]int, len(input))
+	}
+
+	if len(arr) == 0 {
+		return arr
+	}
+
+	offset := len(arr) - len(input)
+	if offset < 0 {
+		_ = fmt.Errorf("[scanAsSplitInt] offset too small: %d", offset)
+	}
+
+	for i, c := range input {
+		var e error
+		arr[offset+i], e = strconv.Atoi(string(c))
+		if e != nil {
+			_ = fmt.Errorf("[scanAsSplitInt] error scanning integer: %s", e)
+		}
+	}
+	return arr
+}
+
+func scanAsString() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+/////////////////////////////
+// Sort
+/////////////////////////////
+
+func reverseString(s string) string {
+	b := []byte(s)
+	size := len(b)
+	// あまりは切り捨てられる
+	for i := 0; i < size/2; i++ {
+		b[i], b[size-1-i] = b[size-1-i], b[i]
+	}
+	return string(b)
+}
+
+func mergeSort(unsorted []int, isAsc bool) []int {
+	if len(unsorted) < 2 {
+		return unsorted
+	}
+
+	mid := len(unsorted) / 2
+
+	// 再帰的に関数を実行してソート済のスライスを返す
+	left := mergeSort(unsorted[:mid], isAsc)
+	right := mergeSort(unsorted[mid:], isAsc)
+
+	sorted := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+
+	// マージ
+	for i < len(left) && j < len(right) {
+		if (isAsc && left[i] <= right[j]) || (!isAsc && left[i] >= right[j]) {
+			sorted = append(sorted, left[i])
+			i++
+		} else {
+			sorted = append(sorted, right[j])
+			j++
+		}
+	}
+
+	// 残りの要素を追加
+	sorted = append(sorted, left[i:]...)
+	sorted = append(sorted, right[j:]...)
+
+	return sorted
+}
+
+/////////////////////////////
+// distinct
+/////////////////////////////
+
+func distinct[T comparable](dup []T) []T {
+	m := make(map[T]struct{}, len(dup))
+	var uniq []T
+	for _, e := range dup {
+		if _, ok := m[e]; ok {
+			continue
+		}
+		m[e] = struct{}{}
+		uniq = append(uniq, e)
+	}
+	return uniq
+}
+
+/////////////////////////////
+// Config
+/////////////////////////////
+
+func flush() {
+	e := w.Flush()
+	if e != nil {
+		_ = fmt.Errorf("[flush] error flushing: %s", e)
+	}
+}
